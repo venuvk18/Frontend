288f32edd66496e7ccbda7f0893e6506a6aa4e3c
Venuvk18 <saiv7926@gmail.com>
2022-12-15 16:25:56 +0530

    ok

--- Frontend/Java Script/one.js
+++ Frontend/Java Script/one.js
@@ -1,9 +1,50 @@
-var ename='venu'
+let ename='venu'
 console.log(ename)
-var marks=20
+let marks=20
 console.log(marks)
 console.log(typeof marks)
 console.log(typeof ename)
 console.log('hello world')
+let marks1=80
+console.log(marks)
+console.log(typeof marks)
+let virat=(2<1)
+console.log(virat)
+console.log(typeof virat)
+let kohli;
+console.log(kohli)
+console.log(typeof kohli)
+let akki=null;
+console.log(akki)
+
+// Arrays
+let biodata= ['Sai',22,22,]
+console.log(biodata)
+console.log(biodata[0])
+console.log(biodata[1])
+console.log(biodata[2])
+console.log(typeof biodata)
+biodata[1]=23
+console.log(biodata)
+biodata.pop()
+console.log(biodata)
+
+//object
+
+let biodata1= {
+    ename:'venu',
+    age:21,
+    gender: 'Male',
+    age:21
+}
+console.log(biodata1)
+console.log(typeof biodata1)
+biodata1.age=40
+console.log(biodata1)
+delete biodata1.age
+console.log(biodata1)
+
+
+
 
 
