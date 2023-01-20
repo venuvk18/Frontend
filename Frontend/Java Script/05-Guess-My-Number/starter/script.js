a0091bad537cd5e8b049805e9b0f9479ebe85d27
Venuvk18 <saiv7926@gmail.com>
2023-01-20 12:51:57 +0530

    ok

--- Frontend/Java Script/05-Guess-My-Number/starter/script.js
+++ Frontend/Java Script/05-Guess-My-Number/starter/script.js
@@ -3,26 +3,44 @@ let secretNumber = Math.trunc(Math.random()*20)+1
  
 let score = 20
 let highScore = 0
+let displayCode = function(message){
+    document.querySelector('.message').textContent=message;
+}
+
 document.querySelector('.check').addEventListener('click',function(){
     let guess= Number(document.querySelector('.guess').value);
     console.log(typeof(guess),guess);
    
 
 if(!guess){
-    document.querySelector('.message').textContent = 'Yoo Fool Give A Number'
+   // document.querySelector('.message').textContent = 'Yoo Fool Give A Number'
+   displayCode('Yoo Fool Give A Number');
 }
     if(guess === secretNumber ) {
-        document.querySelector('.message').textContent = 'Congrats you won'
+        //document.querySelector('.message').textContent = 'Congrats you won'
+        displayCode('Congrats you won');
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.number').style.width = '30rem';
         document.querySelector('.number').textContent = secretNumber;
         
         if(score>highScore){
             highScore=score
-            document.querySelector('.highscore').textContent = highScore;
+            document.querySelector('.highscore').textContent = highScore;     
+        }
+    
+    }
+    else if(guess !== secretNumber) {
+       if(score>1){
+       displayCode( guess>secretNumber? 'Too high':'Too low');
+        score--
+        document.querySelector('.score').textContent = score
+    } else{
+            document.querySelector('.message').textContent = 'You Lost the game' 
+            document.querySelector('.score').textContent =0
         }
     }
-    else if(guess>secretNumber){
+  //  }
+  /*  else if(guess>secretNumber){
         if(score>1){
     document.querySelector('.message').textContent = 'Too high'
     score--
@@ -41,7 +59,7 @@ if(!guess){
             document.querySelector('.message').textContent = 'You Lost the game' 
             document.querySelector('.score').textContent =0
         }
-        }
+        }*/
     }) 
         document.querySelector('.again').addEventListener('click',function(){
             let score = 20   
