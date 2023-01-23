9bca52fda5c3cb3283af5fdfa1dfdc05b4268f18
Venuvk18 <saiv7926@gmail.com>
2023-01-23 11:45:21 +0530

    ok

--- Frontend/Java Script/06-Modal/starter/script.js
+++ Frontend/Java Script/06-Modal/starter/script.js
@@ -2,8 +2,26 @@
  let openModal =  document.querySelector('.modal');
  let overlay   =  document.querySelector('.overlay');
  let btnCloseModal = document.querySelector('.close-modal');
- let btnOpenModal  = document.querySelectorAll('.show-modal');
- console.log(btnOpenModal);
+ let btnsOpenModal  = document.querySelectorAll('.show-modal');
+ 
+ let OopenModal = function(){
+    openModal.classList.remove('hidden');
+    overlay.classList.remove('hidden');}
+ let closeModal = function(){
+    openModal.classList.add('hidden');
+     overlay.classList.add('hidden');
+     }
+ for( let i=0;i<btnsOpenModal.length;i++){
+ btnsOpenModal[i].addEventListener('click',OopenModal);
+ }
+ 
+btnCloseModal.addEventListener('click',closeModal,);
+overlay.addEventListener('click',closeModal);
 
- for( let i=0;i<btnOpenModal.length;i++)
- console.log(btnOpenModal[i].textContent);
+document.addEventListener('keydown',function(e){
+   console.log(e.key);
+
+   if(e.key === 'Escape' && !openModal.classList.contains('hidden')){
+      closeModal();
+   }
+})
